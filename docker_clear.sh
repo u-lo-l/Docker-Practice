069d6b1c031799c524186915de1d9055e90c317f
ulol_inception <jeffk0408@naver.com>
2023-01-17 16:28:24 +0900

    delete unnecessary file

--- docker_clear.sh
+++
@@ -1,8 +0,0 @@
-sudo docker volume rm -f db_vol;
-sudo docker volume rm -f wp_vol;
-sudo docker volume create db_vol;
-sudo docker volume create wp_vol;
-sudo docker image prune -f;
-sudo docker image rm -f inception:mariadb;
-sudo docker image rm -f inception:wordpress;
-sudo docker image rm -f inception:nginx;
